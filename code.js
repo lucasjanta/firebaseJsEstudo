769a8d3dbc5b086fe779a1b71e49e9e2775d8083
lucasjanta <lucastyle2014@gmail.com>
2023-09-03 15:33:32 -0300

    Aulas Firebase + Javascript

---
+++ code.js
@@ -0,0 +1,64 @@
+function onChangeEmail() {
+    toggleButtonsDisabled();
+    toggleEmailErrors();
+}
+
+function onChangePassword() {
+    toggleButtonsDisabled();
+    togglePasswordErrors();
+}
+
+function login(){
+    window.location.href = 'home.html';
+    /* console.log('### window', window);
+    console.log('### window, location', window.location); */
+}
+
+function isEmailValid() {
+    const email = form.email().value;
+    if (!email) {
+        return false;
+    }
+    return validateEmail(email);
+}
+
+function toggleEmailErrors() {
+    const email = form.email().value;
+    form.emailRequiredError().style.display = email ? "none" : "block";
+    form.emailInvalidError().style.display = validateEmail(email) || !email ? "none" : "block";
+}
+
+function togglePasswordErrors() {
+    const password = form.password().value;
+    form.passwordRequiredError().style.display = password ? "none" : "block";
+}
+
+function toggleButtonsDisabled() {
+    const emailValid = isEmailValid();
+    form.recoverPasswordButton().disabled = !emailValid;
+
+    const passwordValid = isPasswordValid();
+    form.loginButton().disabled = !emailValid || !passwordValid;
+    
+}
+
+function isPasswordValid() {
+    const password = form.password().value;
+    if (!password) {
+        return false;
+    }
+    return true;
+}
+
+
+
+const form = {
+    email: () => document.getElementById('email'),
+    emailInvalidError: () => document.getElementById('email-invalid-error'),
+    emailRequiredError: () => document.getElementById('email-required-error'),
+    loginButton: () => document.getElementById('login-button'),
+    password: () => document.getElementById('password'),
+    passwordRequiredError: () => document.getElementById('password-required-error'),
+    recoverPasswordButton: () => document.getElementById('recover-password-button'),
+    
+}
