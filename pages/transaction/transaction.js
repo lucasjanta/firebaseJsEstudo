2abf8fc025892227aa3e7fb94f463c85d4cbb017
lucasjanta <lucastyle2014@gmail.com>
2023-09-24 21:29:08 -0300

    Aula 24 Finalizada

---
+++ pages/transaction/transaction.js
@@ -0,0 +1,91 @@
+function saveTransaction(){
+    showLoading();
+
+    const transaction = createTransaction();
+
+    firebase.firestore()
+    .collection('transactions')
+    .add(transaction)
+    .then(() => {
+        hideLoading();
+        window.location.href = '../home/home.html';
+    })
+    .catch(() => {
+        hideLoading();
+        alert('Erro ao salvar transação');
+    })
+}
+
+function createTransaction(){
+    return {
+        type: form.typeExpense().checked ? 'expense' : 'income',
+        date: form.date().value,
+        money: {
+            currency: form.currency().value,
+            value: parseFloat(form.value().value)
+        },
+        transactionType: form.transactionType().value,
+        description: form.description().value,
+        user: {
+            uid: firebase.auth().currentUser.uid
+        }
+    }
+}
+
+
+function onChangeDate() {
+    const date = form.date().value;
+    form.dateRequiredError().style.display = !date ? 'block' : 'none';
+    toggleSaveButtonDisabled();
+}
+
+function onChangeValue() {
+    const value = form.value().value;
+    form.valueRequiredError().style.display = !value ? 'block' : 'none';
+    form.valueLessOrEqualToZeroError().style.display = value <= 0 ? 'block' : 'none';
+    toggleSaveButtonDisabled();
+}
+
+function onChangeTransactionType() {
+    const transactionType = form.transactionType().value;
+    form.transactionTypeRequiredError().style.display = !transactionType ? 'block' : 'none';
+    toggleSaveButtonDisabled();
+}
+
+function toggleSaveButtonDisabled(){
+    form.saveButton().disabled = !isFormValid();
+}
+
+function isFormValid(){
+    const date = form.date().value;
+    if (!date) {
+        return false;
+    }
+
+    const value = form.value().value;
+    if (!value || value <= 0) {
+        return false;
+    }
+
+    const transactionType = form.transactionType().value;
+    if (!transactionType) {
+        return false;
+    }
+        
+    return true;
+}
+
+const form = {
+    currency: () => document.getElementById('currency'),
+    date: () => document.getElementById('date'),
+    description: () => document.getElementById('description'),
+    dateRequiredError: () => document.getElementById('date-required-error'),
+    saveButton: () => document.getElementById('save-button'),
+    transactionType: () => document.getElementById('transaction-type'),
+    transactionTypeRequiredError: () => document.getElementById('transaction-type-required-error'),
+    typeExpense: () => document.getElementById('expense'),
+    value: () => document.getElementById('value'),
+    valueRequiredError: () => document.getElementById('value-required-error'),
+    valueLessOrEqualToZeroError: () => document.getElementById('value-less-or-equal-to-zero-error')
+
+}
